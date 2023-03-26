1a9d29e21512e77d19237b38bce1be6f88d2fec4
Simon Hartcher <simon@simonhartcher.com>
2023-03-26 15:45:27 +1100

    refactor(LifeBar): remove unused code

--- LifeBar.gd
+++ LifeBar.gd
@@ -1,14 +1,7 @@
 extends TextureProgressBar
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	value = 100
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 	
 func update(health):
 	print(health)
