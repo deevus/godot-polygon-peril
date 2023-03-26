7c3f180903dff6fede1127699260f23847c70a01
Simon Hartcher <simon@simonhartcher.com>
2023-03-26 16:27:57 +1100

    feat(ui): use shader to update lifebar opacity on mouseover

--- LifeBar.gd
+++ LifeBar.gd
@@ -1,6 +1,13 @@
 extends TextureProgressBar
 
+@export var shader_material: ShaderMaterial;
+
+@onready var color_rect = $ColorRect
+
 func _ready():
+	_init_material()
+	_connect_signals()
+
 	value = 100
 	
 func update(health):
@@ -10,3 +17,24 @@ func update(health):
 	# value = 100
 	value = health * 2
 	print(value)
+
+func _init_material() -> void:
+	material = shader_material
+	color_rect.material = shader_material
+
+func _connect_signals() -> void:
+	color_rect.mouse_entered.connect(_on_mouse_entered)
+	color_rect.mouse_exited.connect(_on_mouse_exited)
+
+func set_shader_opacity(opacity: float) -> void:
+	shader_material.set_shader_parameter("opacity", opacity)
+
+func _on_mouse_entered():
+	var current_opacity = shader_material.get_shader_parameter("opacity")
+	var tween = create_tween()
+	tween.tween_method(set_shader_opacity, current_opacity, 0.1, 0.15)
+
+func _on_mouse_exited():
+	var current_opacity = shader_material.get_shader_parameter("opacity")
+	var tween = create_tween()
+	tween.tween_method(set_shader_opacity, current_opacity, 1.0, 0.15)
