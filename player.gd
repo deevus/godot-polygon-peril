ce678cf29d561170efceabe3c620812aacf6fdb0
Simon Hartcher <simon@simonhartcher.com>
2023-03-26 15:37:52 +1100

    refactor(player): use new syntax for emitting player signals

--- player.gd
+++ player.gd
@@ -5,17 +5,9 @@ signal hit
 
 @onready var death_sound = $DeathExplosion
 
-# Called when the node enters the scene tree for the first time.
-func _ready(): 
-	super();
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	super(delta);
-
 func take_damage(damage: int, _source: Object):
 	health = max(health - damage, 0);
-	emit_signal('hit', health)
+	hit.emit(health)
 
 func die():
-	emit_signal("gameover")
+	gameover.emit()
